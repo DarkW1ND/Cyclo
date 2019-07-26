8a7fa62949e00cff7a2a2585f4e5bb3f94dfe3be
Joel Bond <joelbond@JOELS-PC.local>
2019-07-26 08:51:47 -0600

    fix(ping): remove duplicate declaration of discord, fix ping command

--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -3,21 +3,30 @@ const fs = require("fs");
 const config = require("../../config.json");
 const vars = require("../../variables.json");
 
-module.exports = { 
-    config: {
-        name: "ping",
-        description: "PONG! Displays the api & bot latency",
-        usage: "c!ping",
-        category: "miscellaneous",
-        accessableby: "Members"
-    },
-    run: async (client, message, args) => {
-
-
-    let response = new Discord.RichEmbed()
-        .setTitle(`Test`)
-        .setDescription(`Test`)
-        .addField(`Test`)
-    message.channel.send(response);
-}
-}
+module.exports = {
+  config: {
+    name: "ping",
+    description: "PONG! Displays the api & bot latency",
+    usage: "c!ping",
+    category: "miscellaneous",
+    accessableby: "Members"
+  },
+  run: async (client, message, args) => {
+    exports.run = async (client, message, args, command) => {
+      const m = await message.channel.send("Pong?");
+      m.edit({
+        embed: {
+          author: {
+            name: client.user.username,
+            icon_url: client.user.avatarURL
+          },
+          description: `Command Latency: **${m.createdTimestamp -
+            message.createdTimestamp}ms**\nAPI Latency: **${Math.round(
+            client.ping
+          )}ms**`,
+          timestamp: new Date()
+        }
+      });
+    };
+  }
+};
