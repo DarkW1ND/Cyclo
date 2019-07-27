204aa0dc60d53f41fd65199665d29ff079986882
DarkW1ND <41177824+DarkW1ND@users.noreply.github.com>
2019-07-27 19:02:28 -0400

    Fixing eval!

--- commands/owner-only/eval.js
+++ commands/owner-only/eval.js
@@ -1,70 +1,56 @@
-const vars = require("../../variables.json");
+//Ale! Eval command. DrastDev 2019 https://drastdev.com/
 const Discord = require("discord.js");
+const steam = require('../supportteam.json');
 
-function clean(text) {
-  if (typeof text === "string")
-    return text
-      .replace(/`/g, "`" + String.fromCharCode(8203))
-      .replace(/@/g, "@" + String.fromCharCode(8203));
-  else return text;
-}
+exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
 
-module.exports = {
-  config: {
-    name: "eval",
-    description: "Run javascript commands from within Discord",
-    usage: "c!eval",
-    category: "owner-only",
-    accessableby: "Owners Only"
-  },
-  run: async (client, message, args, guild, member) => {
-    const errorm = client.emojis.get("554031332498604052");
-    const okm = client.emojis.get("554031332301471761");
+  const error = client.emojis.get("554031332498604052");
+  const checkmark = client.emojis.get("554031332301471761");
 
-    let response = new Discord.RichEmbed()
-    .setTitle(`${errorm} Oops!`)
-    .setDescription(`You must be the bot owner to use this!`)
-    .setColor(vars.error)
-
-    if (!message.author.id === "475673195706449933") return message.channel.send(response)
-    if (!message.author.id === "575865352186101761") return message.channel.send(response);
-    const code = args.join(" ");
-
-    let fo = new Discord.RichEmbed()
-    .setTitle(`${errorm} Error!`)
-    .setDescription(`**ERROR:** \n\`\`\`xl\nForbidden output\n\`\`\``)
-    .setColor(vars.error)
-
-    let be = new Discord.RichEmbed()
-    .setTitle(`${errorm} Error!`)
-    .setDescription(`**ERROR:** \n\`\`\`xl\nBlacklisted output\n\`\`\``)
-    .setColor(vars.error)
-
-    if(message.content.toLowerCase().includes('token'||'TOKEN')) return message.channel.send(fo);
-    if(message.content.toLowerCase().includes('setInterval(function(){message.channel.send')) return message.channel.send(be);
-
-    try {
-      let evaled = eval(code);
-
-      if (typeof evaled !== "string") evaled = require("util").inspect(evaled);
-
-
-      let codemsg = new Discord.RichEmbed()
-      .setTitle(`${okm} Evaled!`)
-      .setDescription(`**Result:** \n\`\`\`xl\n${clean(evaled)}\n\`\`\``)
-      .setColor(vars.good) 
-      message.channel.send(codemsg);
-    } catch (err) {
-      let error = new Discord.RichEmbed()
-      .setTitle(`${errorm} Error!`)
-      .setDescription(`**ERROR:** \n\`\`\`xl\n${clean(err)}\n\`\`\``)
-      .setColor(vars.error)
-      message.channel.send(error);
-    }
+  function clean(text) {
+    if (typeof text === "string")
+      return text
+        .replace(/`/g, "`" + String.fromCharCode(8203))
+        .replace(/@/g, "@" + String.fromCharCode(8203));
+    else return text;
   }
-};
 
-exports.conf = {
-  category: `Owner Only`,
-  aliases: [""]
+  const code = args.join(" ");
+    
+  const forbidden = new Discord.RichEmbed()
+  .setColor("b722e5")
+  .setDescription(`${error} ERROR\n\`\`\`Forbidden output\`\`\``)
+
+  if (
+    code.includes("token") ||
+    code.includes("destroy") ||
+    code.includes("config") ||
+    code.includes("exit")
+  )
+    return message.channel.send(forbidden);
+
+  const poa = new Discord.RichEmbed()
+  .setColor(0xb722e5)
+  .setDescription(`${error} This command is owner only!`)
+
+  if(!steam[message.author.id]){
+    return message.channel.send(poa)
 };
+  try {
+    const code = args.join(" ");
+    let evaled = eval(code);
+
+    if (typeof evaled !== "string") evaled = require("util").inspect(evaled);
+
+    const result = new Discord.RichEmbed()
+    .setColor("b722e5")
+    .setDescription(`${checkmark} Result\n\`\`\`\n${clean(evaled)}\`\`\``)
+
+    message.channel.send(result);
+  } catch (err) {
+    const resulterr = new Discord.RichEmbed()
+    .setColor("b722e5")
+    .setDescription(`${error} ERROR\n\`\`\`\n${clean(err)}\`\`\``)
+    message.channel.send(resulterr);
+  }
+  };
