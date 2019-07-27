88281a831288fa43e276bc8ada560b3acb248d99
xItsDanger <andresitolipo@gmail.com>
2019-07-27 23:55:41 +0200

    fixed help and completly hide token

--- commands/owner-only/eval.js
+++ commands/owner-only/eval.js
@@ -35,7 +35,9 @@ module.exports = {
     .setDescription(`**ERROR:** \n\`\`\`xl\nForbidden output\n\`\`\``)
     .setColor(vars.error)
 
-    if(message.content.includes(`token TOKEN`)) return message.channel.send(fo);
+    if(message.content.includes('token'||'TOKEN')) {
+       message.channel.send(fo);
+    }
 
     try {
       let evaled = eval(code);
